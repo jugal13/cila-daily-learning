bd4cca58e8d60f96fd965a05fedb7e779c3bdb1a
Jugal <jugaldeepak@gmail.com>
2021-03-10 23:31:46 +0530

    color flipper js

---
+++ js-color-flipper/main.js
@@ -0,0 +1,64 @@
+let simple = true;
+const bodyText = document.getElementById("body-text");
+const links = document.querySelectorAll(".btn");
+console.log(bodyText);
+
+function generate() {
+  const bgColor = simple ? generateSimple() : generateHex();
+  bodyText.style.color = bgColor;
+  document.body.style.backgroundColor = bgColor;
+  bodyText.innerText = `Background Color: ${bgColor}`;
+}
+
+function generateSimple() {
+  colors = ["Red", "Blue", "Green", "Yellow"];
+
+  let index = getRandomNumber(colors.length);
+
+  return colors[index];
+}
+
+function generateHex() {
+  values = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+
+  let hexColor = "#";
+
+  for (let i = 0; i < 6; i++) {
+    hexColor += values[getRandomNumber(values.length)];
+  }
+
+  return hexColor;
+}
+
+function getRandomNumber(length) {
+  return Math.floor(Math.random() * length);
+}
+
+function setSimple() {
+  links[1].classList.remove(["active"]);
+  simple = true;
+  links[0].classList.add(["active"]);
+}
+
+function setHex() {
+  links[0].classList.remove(["active"]);
+  simple = false;
+  links[1].classList.add(["active"]);
+}
