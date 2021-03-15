9711c85da2784c59d4bd795e1688c95360282fed
Jugal <jugaldeepak@gmail.com>
2021-03-15 17:21:38 +0530

    smackchat part 2 (users page UI)

--- smackchat/src/pages/PageUsers.vue
+++ smackchat/src/pages/PageUsers.vue
@@ -1,11 +1,64 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Users Page</h5>
+  <q-page class="flex q-pa-md">
+    <q-list
+      class="full-width"
+      separator
+    >
+      <q-item
+        v-for="user in users"
+        :key="user.id"
+        clickable
+        v-ripple
+        to="/chat"
+      >
+        <q-item-section avatar>
+          <q-avatar
+            color="primary"
+            text-color="white"
+          >
+            {{ user.name[0] }}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge :color="user.online ? 'light-green-5' : 'grey-4' ">
+            {{ user.online ? 'Online' : 'Offline'}}
+          </q-badge>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
   </q-page>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Danny",
+          online: true,
+        },
+        {
+          id: 2,
+          name: "Jim",
+          online: false,
+        },
+        {
+          id: 3,
+          name: "Lucy",
+          online: true,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
