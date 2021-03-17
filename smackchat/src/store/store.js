7f57343c7102bc3b9aa77ef3771b4a8d664d1577
Jugal <jugaldeepak@gmail.com>
2021-03-17 15:05:00 +0530

    smackchat part 8 (login, authstate, userdetails)

--- smackchat/src/store/store.js
+++ smackchat/src/store/store.js
@@ -1,15 +1,20 @@
 import { firebaseAuth, firebaseDb } from "boot/firebase";
 
-const state = {};
+const state = {
+  userDetails: {}
+};
 
-const mutations = {};
+const mutations = {
+  setUserDetails(state, payload) {
+    state.userDetails = payload;
+  }
+};
 
 const actions = {
   registerUser({}, payload) {
     firebaseAuth
       .createUserWithEmailAndPassword(payload.email, payload.password)
       .then(authCredential => {
-        console.log(authCredential);
         firebaseDb.ref("users/" + authCredential.user.uid).set({
           name: payload.name,
           email: payload.email,
@@ -25,12 +30,22 @@ const actions = {
     firebaseAuth
       .signInWithEmailAndPassword(payload.email, payload.password)
       .then(authCredential => {
-        console.log(authCredential);
-        firebaseDb
-          .ref("users/" + authCredential.user.uid)
-          .set({ online: true });
+        console.log("logged in");
       })
       .catch(error => console.error(error));
+  },
+
+  handleAuthStateChanged({ commit }) {
+    firebaseAuth.onAuthStateChanged(user => {
+      if (user) {
+        firebaseDb.ref("users/" + user.uid).once("value", snapshot => {
+          const userDetails = snapshot.val();
+          commit("setUserDetails", { ...userDetails, userId: user.uid });
+        });
+      } else {
+        commit("setUserDetails", {});
+      }
+    });
   }
 };
 
